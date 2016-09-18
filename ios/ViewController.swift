77db6c0e7275ceabf58255d9612336b66a8147d2
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2016-09-18 10:50:41 +0700

    Binding landing screen with viewcontroller

--- ios/ViewController.swift
+++ ios/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var logoConstraint: NSLayoutConstraint!
+    @IBOutlet weak var signinFormView: UIView!
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var signinButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
