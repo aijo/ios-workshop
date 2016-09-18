3a0d220131ad52f981864cf24b39d3e0e742c03b
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2016-09-18 20:57:40 +0700

    Create and setup tableviewcell as dynamic height

---
+++ ios/FeedCell.swift
@@ -0,0 +1,38 @@
+//
+//  FeedCell.swift
+//  ios
+//
+//  Created by Chanintorn Asavavichairoj on 9/18/2559 BE.
+//  Copyright © 2559 JO. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    
+    private let USER_IMAGE_SIZE:CGFloat = 35
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        userImageView.backgroundColor = UIColor.init(rgba: "#EEEEEE")
+        photoImageView.backgroundColor = UIColor.init(rgba: "#EEEEEE")
+        userImageView.layer.cornerRadius = USER_IMAGE_SIZE/2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
